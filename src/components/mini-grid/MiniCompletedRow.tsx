4e127b4d604195d21276c1ed789af52a500f20cc
Dionysos Dajka <diondiondion@gmail.com>
2022-01-15 21:33:04 +0100

    Add German wordlist

--- src/components/mini-grid/MiniCompletedRow.tsx
+++ src/components/mini-grid/MiniCompletedRow.tsx
@@ -1,18 +1,24 @@
-import { getGuessStatuses } from "../../lib/statuses";
-import { MiniCell } from "./MiniCell";
+import {getGuessStatuses} from '../../lib/statuses';
 
 type Props = {
-  guess: string;
+	guess: string;
 };
 
-export const MiniCompletedRow = ({ guess }: Props) => {
-  const statuses = getGuessStatuses(guess);
+export const MiniCompletedRow = ({guess}: Props) => {
+	const statuses = getGuessStatuses(guess);
 
-  return (
-    <div className="flex justify-center mb-1">
-      {guess.split("").map((letter, i) => (
-        <MiniCell key={i} status={statuses[i]} />
-      ))}
-    </div>
-  );
+	return (
+		<div className="flex justify-center">
+			{guess.split('').map((_, i) => {
+				switch (statuses[i]) {
+					case 'absent':
+						return '⬜';
+					case 'present':
+						return '🟨';
+					case 'correct':
+						return '🟩';
+				}
+			})}
+		</div>
+	);
 };
