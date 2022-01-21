3cec4a3c54fa850759392f9dd7c64e291968a707
Dionysos Dajka <diondiondion@gmail.com>
2022-01-21 19:11:01 +0100

    Support keyboard entry for umlauts

--- src/components/keyboard/Keyboard.tsx
+++ src/components/keyboard/Keyboard.tsx
@@ -1,90 +1,93 @@
-import {KeyValue} from '../../lib/keyboard';
-import {getStatuses} from '../../lib/statuses';
-import {Key} from './Key';
-import {useEffect} from 'react';
+import { KeyValue } from '../../lib/keyboard'
+import { getStatuses } from '../../lib/statuses'
+import { Key } from './Key'
+import { useEffect } from 'react'
 
 type Props = {
-	onChar: (value: string) => void;
-	onDelete: () => void;
-	onEnter: () => void;
-	guesses: string[];
-};
+  onChar: (value: string) => void
+  onDelete: () => void
+  onEnter: () => void
+  guesses: string[]
+}
 
-export const Keyboard = ({onChar, onDelete, onEnter, guesses}: Props) => {
-	const charStatuses = getStatuses(guesses);
+export const Keyboard = ({ onChar, onDelete, onEnter, guesses }: Props) => {
+  const charStatuses = getStatuses(guesses)
 
-	const onClick = (value: KeyValue) => {
-		if (value === 'ENTER') {
-			onEnter();
-		} else if (value === 'DELETE') {
-			onDelete();
-		} else {
-			onChar(value);
-		}
-	};
+  const onClick = (value: KeyValue) => {
+    if (value === 'ENTER') {
+      onEnter()
+    } else if (value === 'DELETE') {
+      onDelete()
+    } else {
+      onChar(value)
+    }
+  }
 
-	useEffect(() => {
-		const listener = (e: KeyboardEvent) => {
-			if (e.code === 'Enter') {
-				onEnter();
-			} else if (e.code === 'Backspace') {
-				onDelete();
-			} else {
-				const key = e.key.toUpperCase();
-				if (key.length === 1 && key >= 'A' && key <= 'Z') {
-					onChar(key);
-				}
-			}
-		};
-		window.addEventListener('keyup', listener);
-		return () => {
-			window.removeEventListener('keyup', listener);
-		};
-	}, [onEnter, onDelete, onChar]);
+  useEffect(() => {
+    const listener = (e: KeyboardEvent) => {
+      if (e.code === 'Enter') {
+        onEnter()
+      } else if (e.code === 'Backspace') {
+        onDelete()
+      } else {
+        const key = e.key.toUpperCase()
+        if (
+          (key.length === 1 && key >= 'A' && key <= 'Z') ||
+          ['Ä', 'Ü', 'Ö'].includes(key)
+        ) {
+          onChar(key)
+        }
+      }
+    }
+    window.addEventListener('keyup', listener)
+    return () => {
+      window.removeEventListener('keyup', listener)
+    }
+  }, [onEnter, onDelete, onChar])
 
-	return (
-		<div>
-			<div className="flex justify-center mb-1">
-				<Key value="Q" onClick={onClick} status={charStatuses['Q']} />
-				<Key value="W" onClick={onClick} status={charStatuses['W']} />
-				<Key value="E" onClick={onClick} status={charStatuses['E']} />
-				<Key value="R" onClick={onClick} status={charStatuses['R']} />
-				<Key value="T" onClick={onClick} status={charStatuses['T']} />
-				<Key value="Z" onClick={onClick} status={charStatuses['Z']} />
-				<Key value="U" onClick={onClick} status={charStatuses['U']} />
-				<Key value="I" onClick={onClick} status={charStatuses['I']} />
-				<Key value="O" onClick={onClick} status={charStatuses['O']} />
-				<Key value="P" onClick={onClick} status={charStatuses['P']} />
-				<Key value="Ü" onClick={onClick} status={charStatuses['Ü']} />
-			</div>
-			<div className="flex justify-center mb-1 sm:ml-4">
-				<Key value="A" onClick={onClick} status={charStatuses['A']} />
-				<Key value="S" onClick={onClick} status={charStatuses['S']} />
-				<Key value="D" onClick={onClick} status={charStatuses['D']} />
-				<Key value="F" onClick={onClick} status={charStatuses['F']} />
-				<Key value="G" onClick={onClick} status={charStatuses['G']} />
-				<Key value="H" onClick={onClick} status={charStatuses['H']} />
-				<Key value="J" onClick={onClick} status={charStatuses['J']} />
-				<Key value="K" onClick={onClick} status={charStatuses['K']} />
-				<Key value="L" onClick={onClick} status={charStatuses['L']} />
-				<Key value="Ö" onClick={onClick} status={charStatuses['Ö']} />
-				<Key value="Ä" onClick={onClick} status={charStatuses['Ä']} />
-			</div>
-			<div className="flex justify-center">
-				<Key width={65.4} value="ENTER" onClick={onClick}>
-					Eingabe
-				</Key>
-				<Key value="Y" onClick={onClick} status={charStatuses['Y']} />
-				<Key value="X" onClick={onClick} status={charStatuses['X']} />
-				<Key value="C" onClick={onClick} status={charStatuses['C']} />
-				<Key value="V" onClick={onClick} status={charStatuses['V']} />
-				<Key value="B" onClick={onClick} status={charStatuses['B']} />
-				<Key value="N" onClick={onClick} status={charStatuses['N']} />
-				<Key value="M" onClick={onClick} status={charStatuses['M']} />
-				<Key width={65.4} value="DELETE" onClick={onClick}>
-					Löschen
-				</Key>
-			</div>
-		</div>
-	);
-};
+  return (
+    <div>
+      <div className="flex justify-center mb-1">
+        <Key value="Q" onClick={onClick} status={charStatuses['Q']} />
+        <Key value="W" onClick={onClick} status={charStatuses['W']} />
+        <Key value="E" onClick={onClick} status={charStatuses['E']} />
+        <Key value="R" onClick={onClick} status={charStatuses['R']} />
+        <Key value="T" onClick={onClick} status={charStatuses['T']} />
+        <Key value="Z" onClick={onClick} status={charStatuses['Z']} />
+        <Key value="U" onClick={onClick} status={charStatuses['U']} />
+        <Key value="I" onClick={onClick} status={charStatuses['I']} />
+        <Key value="O" onClick={onClick} status={charStatuses['O']} />
+        <Key value="P" onClick={onClick} status={charStatuses['P']} />
+        <Key value="Ü" onClick={onClick} status={charStatuses['Ü']} />
+      </div>
+      <div className="flex justify-center mb-1 sm:ml-4">
+        <Key value="A" onClick={onClick} status={charStatuses['A']} />
+        <Key value="S" onClick={onClick} status={charStatuses['S']} />
+        <Key value="D" onClick={onClick} status={charStatuses['D']} />
+        <Key value="F" onClick={onClick} status={charStatuses['F']} />
+        <Key value="G" onClick={onClick} status={charStatuses['G']} />
+        <Key value="H" onClick={onClick} status={charStatuses['H']} />
+        <Key value="J" onClick={onClick} status={charStatuses['J']} />
+        <Key value="K" onClick={onClick} status={charStatuses['K']} />
+        <Key value="L" onClick={onClick} status={charStatuses['L']} />
+        <Key value="Ö" onClick={onClick} status={charStatuses['Ö']} />
+        <Key value="Ä" onClick={onClick} status={charStatuses['Ä']} />
+      </div>
+      <div className="flex justify-center">
+        <Key width={65.4} value="ENTER" onClick={onClick}>
+          Eingabe
+        </Key>
+        <Key value="Y" onClick={onClick} status={charStatuses['Y']} />
+        <Key value="X" onClick={onClick} status={charStatuses['X']} />
+        <Key value="C" onClick={onClick} status={charStatuses['C']} />
+        <Key value="V" onClick={onClick} status={charStatuses['V']} />
+        <Key value="B" onClick={onClick} status={charStatuses['B']} />
+        <Key value="N" onClick={onClick} status={charStatuses['N']} />
+        <Key value="M" onClick={onClick} status={charStatuses['M']} />
+        <Key width={65.4} value="DELETE" onClick={onClick}>
+          Löschen
+        </Key>
+      </div>
+    </div>
+  )
+}
