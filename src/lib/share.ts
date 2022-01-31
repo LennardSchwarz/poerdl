a6da6079407e396dbdd87988eeed817ffbfa15a7
Dionysos Dajka <diondiondion@gmail.com>
2022-01-31 06:16:29 +0100

    Fix forum code bug

--- src/lib/share.ts
+++ src/lib/share.ts
@@ -12,16 +12,17 @@ export const shareStatus = (guesses: string[], lost: boolean) => {
 
 export const shareStatusWithBBCode = (guesses: string[], lost: boolean) => {
   navigator.clipboard.writeText(
-    '[Wörtchen](https://woertchen.sofacoach.de) ' + solutionIndex + ' ' + lost
-      ? 'X'
-      : guesses.length +
-          '/6\n\n' +
-          guesses
-            .map(
-              (guess) =>
-                `${generateEmojiGridLine(guess)} [spoiler]${guess}[/spoiler]`
-            )
-            .join('\n')
+    '[Wörtchen](https://woertchen.sofacoach.de) ' +
+      solutionIndex +
+      ' ' +
+      (lost ? 'X' : guesses.length) +
+      '/6\n\n' +
+      guesses
+        .map(
+          (guess) =>
+            `${generateEmojiGridLine(guess)} [spoiler]${guess}[/spoiler]`
+        )
+        .join('\n')
   )
 }
 
